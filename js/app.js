b3180727660da422d9c98e092b64e303bf99664d
Seth <sethparkerpierce@icloud.com>
2022-09-19 18:20:08 -0400

    added functions

--- js/app.js
+++ js/app.js
@@ -5,13 +5,19 @@
 let glbHours = ['6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
 let salesSection = document.getElementById('cookie-sales');
 console.dir(salesSection);
+
+let headerSection = document.getElementById('cookie-header');
+console.dir(headerSection);
+
+let cookieTop = document.getElementById('cookie-top')
+console.dir(cookieTop);
 // *** Object Literals ***
 
 let seattleCookie = {
   city: 'Seattle',
   minCust: 23,
   maxCust: 65,
-  avgCust: 6.3,
+  avgCookie: 6.3,
   amntCookie: 0,
   totalCookie: 0,
   totalCookieArr: [],
@@ -22,7 +28,7 @@ let tokyoCookie = {
   city: 'Tokyo',
   minCust: 3,
   maxCust: 24,
-  avgCust: 1.2,
+  avgCookie: 1.2,
   amntCookie: 0,
   totalCookie: 0,
   totalCookieArr: [],
@@ -33,7 +39,7 @@ let dubaiCookie = {
   city: 'Dubai',
   minCust: 11,
   maxCust: 38,
-  avgCust: 3.7,
+  avgCookie: 3.7,
   amntCookie: 0,
   totalCookie: 0,
   totalCookieArr: [],
@@ -44,7 +50,7 @@ let parisCookie = {
   city: 'Paris',
   minCust: 20,
   maxCust: 38,
-  avgCust: 2.3,
+  avgCookie: 2.3,
   amntCookie: 0,
   totalCookie: 0,
   totalCookieArr: [],
@@ -55,7 +61,7 @@ let limaCookie = {
   city: 'Lima',
   minCust: 2,
   maxCust: 16,
-  avgCust: 4.6,
+  avgCookie: 4.6,
   amntCookie: 0,
   totalCookie: 0,
   totalCookieArr: [],
@@ -69,19 +75,37 @@ function randomCookie (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function cookieHeader(){
+  let headerElem = document.createElement('header');
+  headerSection.appendChild(headerElem);
+  let h1Elem = document.createElement('h1');
+  h1Elem.textContent= 'Salmon Cookies';
+  headerElem.appendChild(h1Elem);
+  let imgElem = document.createElement('img');
+  imgElem.src = 'img/salmon.png';
+  imgElem.alt = 'picture of a beautiful salmon';
+  imgElem.id = 'salmon';
+  headerElem.appendChild(imgElem);
+
+}
+function cookieHtwo() {
+  let h2Elem = document.createElement('h2');
+  h2Elem.textContent = 'Cookie Sales';
+  cookieTop.appendChild(h2Elem);
+}
 function cookieList(){
   let articleElem = document.createElement('article');
   salesSection.appendChild(articleElem);
 
-  let h2Elem = document.createElement('h2');
-  h2Elem.textContent= this.city;
-  articleElem.appendChild(h2Elem);
+  let h3Elem = document.createElement('h3');
+  h3Elem.textContent= this.city;
+  articleElem.appendChild(h3Elem);
 
   let ulElem = document.createElement('ul');
   articleElem.appendChild(ulElem);
 
   for(let i = 0; i < glbHours.length; i++){
-    this.amntCookie = randomCookie(this.minCust,this.maxCust);
+    this.amntCookie = Math.floor(randomCookie(this.minCust,this.maxCust) * this.avgCookie);
     this.totalCookie = this.totalCookie + this.amntCookie;
     this.totalCookieArr.push(`${glbHours[i]}: ${this.amntCookie} cookies`);
     let liElem = document.createElement('li');
@@ -93,8 +117,11 @@ function cookieList(){
   liElem.textContent = `Total: ${this.totalCookie} cookies`;
   ulElem.appendChild(liElem);
 }
-// **** executeable code ****
 
+
+// **** executeable code ****
+cookieHeader();
+cookieHtwo();
 seattleCookie.renderCookie();
 tokyoCookie.renderCookie();
 dubaiCookie.renderCookie();
