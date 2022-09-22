5c33632e1aa53ee995706092c6da6ccaf1236607
Seth <sethparkerpierce@icloud.com>
2022-09-22 19:18:10 -0400

    added dyinamic tfoot and form functionality

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@
 
 // *** Globals****
 
+
+
+
+
 let glbHours = ['6:00am','7:00am','8:00am','9:00am','10:00am','11:00am','12:00am','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','6:00pm','7:00pm'];
 let salesSection = document.getElementById('cookie-sales');
 console.dir(salesSection);
@@ -85,6 +89,7 @@ function dailytotals(){
   }}
 
 
+
 function tableTop(){
   let theadElem = document.createElement('thead');
   cookieTable.appendChild(tableElem);
@@ -111,11 +116,18 @@ function totalAllDaily(){
     ttlallDaily += ttlArr[i];}
   return ttlallDaily;}
 
+function totalAllDailyNew(){
+  let ttlArrNew = ttlArr.slice(-14);
+  let ttlallDailyNew = 0;
+  for(let i = 0; i < 14; i ++){
+    ttlallDailyNew += ttlArrNew[i];}
+  return ttlallDailyNew;
+}
 
-//   let allDaily = (ttlArr[0] + ttlArr[1]) + (ttlArr[2] + ttlArr[3]) + (ttlArr[4] + ttlArr[5]) + (ttlArr[6] + ttlArr[7]) + (ttlArr[8] + ttlArr[9]) + (ttlArr[10] + ttlArr[11]) + (ttlArr[12] + ttlArr[13]);
 
 function tableBottom() {
   let tfootElem = document.createElement('tfoot');
+  tfootElem.id = 'tfoot';
   tableElem.appendChild(tfootElem);
   let totalFoot = document.createElement('tr');
   tfootElem.appendChild(totalFoot);
@@ -134,6 +146,30 @@ function tableBottom() {
   allTotal.textContent = `${allDaily} cookies`;
   allTotal.classList.add('content');
   totalFoot.appendChild(allTotal);}
+
+function tableBottomNew() {
+  let tfootElem = document.createElement('tfoot');
+  tfootElem.id = 'tfoot';
+  tableElem.appendChild(tfootElem);
+  let totalFoot = document.createElement('tr');
+  tfootElem.appendChild(totalFoot);
+  let totaltitle = document.createElement('td');
+  totaltitle.textContent = ('Totals');
+  totaltitle.classList.add('rowstart');
+  totalFoot.appendChild(totaltitle);
+  let ttlArrNew = ttlArr.slice(-14);
+  for (let i = 0; i < ttlArrNew.length; i++) {
+    let totalTd = document.createElement('td');
+    totalTd.textContent = `${ttlArrNew[i]} cookies`;
+    totalTd.classList.add('content');
+    totalFoot.appendChild(totalTd);
+  }
+  let allDaily = totalAllDailyNew();
+  let allTotal = document.createElement('td');
+  allTotal.textContent = `${allDaily} cookies`;
+  allTotal.classList.add('content');
+  totalFoot.appendChild(allTotal);
+}
 
 // **** prototype method *******
 CityConstruct.prototype.render = function(){
@@ -159,6 +195,31 @@ CityConstruct.prototype.render = function(){
 };
 
 
+let myForm = document.getElementById('frm1');
+
+function renderCities(){for(let i = 0; i <citiesArray.length;i++){
+  citiesArray[i].render();
+}}
+function handleSubmit(event){
+  event.preventDefault();
+  let newCityName = event.target.inputCityName.value;
+  console.log(newCityName);
+  let newMinCust = parseInt(event.target.inputMinCust.value);
+  console.log(newMinCust);
+  let newMaxCust = parseInt(event.target.inputMaxCust.value);
+  console.log(newMaxCust);
+  let newAvgCust = parseInt(event.target.inputAvgCust.value);
+  console.log(newAvgCust);
+  new CityConstruct(newCityName,newMinCust,newMaxCust,newAvgCust).render();
+  let tfootRemove = document.getElementById('tfoot');
+  tfootRemove.remove();
+  dailytotals();
+  tableBottomNew();
+  myForm.reset();
+}
+
+myForm.addEventListener('submit', handleSubmit);
+
 // **** executeable code ****
 new CityConstruct('Seattle',23,65,6.3);
 new CityConstruct('Tokyo',3,24,1.2);
@@ -166,14 +227,16 @@ new CityConstruct('Dubai',11,38,3.7);
 new CityConstruct('Paris',20,38,2.3);
 new CityConstruct('Lima',2,16,4.6);
 
-citiesArray[0].render();
-citiesArray[1].render();
-citiesArray[2].render();
-citiesArray[3].render();
-citiesArray[4].render();
+// citiesArray[0].render();
+// citiesArray[1].render();
+// citiesArray[2].render();
+// citiesArray[3].render();
+// citiesArray[4].render();
 
+renderCities();
 cookieHeader();
 cookieHtwo();
 tableTop();
 dailytotals();
 tableBottom();
+
